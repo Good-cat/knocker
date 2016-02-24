f23f58fa53de0f76cc645db02786e09be6a8c6d2
Mikhail Pehasin <mikhail.pegasin@itmhouse.com>
2016-02-24 13:39:01 +0300

    create ApiController

---
+++ src/AppBundle/Controller/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of knocker project
+ * Created by Mikhail Peghasin
+ * mikhail.pegasin@itmhouse.com
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ApiController extends Controller{
+
+} 
