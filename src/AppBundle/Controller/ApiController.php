b8256851e831861c6e46bdb121a402f235cf79f0
Mikhail Pehasin <mikhail.pegasin@itmhouse.com>
2016-02-24 14:17:35 +0300

    create get-knocker-user action template

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -8,8 +8,30 @@
 namespace AppBundle\Controller;
 
 
+use Application\Sonata\UserBundle\Document\User;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ApiController
+ * @package AppBundle\Controller
+ * @Route("/{apiKey}/api")
+ */
 class ApiController extends FOSRestController{
 
+    /**
+     * @param $userKey
+     * @return mixed|void
+     * @Route("/user/{userKey}")
+     */
+    public function getKnockerUser($userKey)
+    {
+        $user = new User();
+        $user->setUsername($userKey);
+        $view = $this
+            ->view($user, 200)
+            ->setFormat('json');
+
+        return $this->handleView($view);
+    }
 } 
