1ef48e193aae25540e62d2d800d2301fec24b028
Mikhail Pehasin <mikhail.pegasin@itmhouse.com>
2016-02-25 12:05:13 +0300

    add profile controller

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of knocker project
+ * Created by Mikhail Peghasin
+ * mikhail.pegasin@itmhouse.com
+ */
+
+namespace AppBundle\Controller;
+
+use FOS\UserBundle\Controller\ProfileController as FOSProfileController;
+class ProfileController extends FOSProfileController
+{
+    public function
+} 
